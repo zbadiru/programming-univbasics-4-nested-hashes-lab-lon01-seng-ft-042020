5b47d9010cc29f1ca7733929913601754f6f747a
Zakari Badiru <zbadiru@live.co.uk>
2020-03-31 14:53:29 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,6 +36,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+	programmer_hash[:alan_kay][:known_for]
 
 end
 
@@ -55,7 +56,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages].join
 end
 
 def adding_matz
@@ -66,8 +67,11 @@ def adding_matz
 # }
 # return the entire updated hash
 
-	programmer_hash = 
- 		{
+	programmer_hash = {
+	    :yukihiro_matsumoto => {
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]
+	    },
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
@@ -81,7 +85,7 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+programmer_hash
 end
 
 def changing_alan
@@ -103,6 +107,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
+	programmer_hash[:alan_kay][:known_for]="GUI"
+	programmer_hash
 
 end
 
@@ -125,5 +131,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages]<< "Assembly"
+programmer_hash
 end
