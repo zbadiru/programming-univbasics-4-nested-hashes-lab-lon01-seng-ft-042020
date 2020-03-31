8a4436a2a922694cf0d3578f83d3458c465c9afb
Zakari Badiru <zbadiru@live.co.uk>
2020-03-31 14:53:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -131,6 +131,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-programmer_hash[:dennis_ritchie][:languages]<<"Assembly"
+programmer_hash[:dennis_ritchie][:languages]<< "Assembly"
 programmer_hash
 end
