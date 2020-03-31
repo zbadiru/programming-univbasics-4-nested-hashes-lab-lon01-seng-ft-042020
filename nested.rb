7db6f80770782d651140a63df2ac552df2755249
Zakari Badiru <zbadiru@live.co.uk>
2020-03-31 14:39:11 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -20,7 +20,6 @@ end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	programmer_hash[:alan_kay][:known_for]
 	
 	programmer_hash = 
  		{
@@ -37,6 +36,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+	programmer_hash[:alan_kay][:known_for]
 
 end
 
