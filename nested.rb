01593076d6b35a508cc45b3017f5032424896637
Zakari Badiru <zbadiru@live.co.uk>
2020-03-31 14:45:04 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -61,10 +61,10 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#   :languages => ["LISP", "C"]
-# }
+:yukihiro_matsumoto => 
+:known_for => "Ruby",
+:languages => ["LISP", "C"]
+}
 # return the entire updated hash
 
 	programmer_hash = 
